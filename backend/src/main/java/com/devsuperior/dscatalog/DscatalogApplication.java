e8c7e33a9e527d385be0c94f76f9e3643fb5324f
glebson2015 <glebson.lourenco@gmail.com>
2021-02-04 21:07:38 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Está funfnado!");
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
