5dbf18c48e7294c8248f134c23c9c05d12b11156
glebson2015 <glebson.lourenco@gmail.com>
2021-05-27 21:02:11 -0300

    Product insert, update e delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -12,8 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -26,6 +29,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository; //Dependencia
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true) // Não bloqueia o BD apenas por fazer uma leitura // Inserido para transações de leitura
 	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
@@ -56,18 +62,18 @@ public class ProductService {
 	public ProductDTO insert(ProductDTO dto) {
 		
 		Product entity = new Product();
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		
 	}
-	
+		
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
 		
 		try {
 		Product entity = repository.getOne(id);//Método getOne não toca no BD - Instancia um Objeto provisório e só ao salvar que de fato acessará o BD para atualizar
-		//entity.setName(dto.getName());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		
@@ -88,5 +94,23 @@ public class ProductService {
 			throw new DatabaseException("Integrity violation");
 		}
 	}
+	
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for(CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());//getOne sem tocar no Banco de Dados
+			entity.getCategories().add(category);
+		}
+		
+		
+	}
+
 
 }
